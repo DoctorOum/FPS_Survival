1fe5fca604ea1fbbcb53160be15d67bce0cc3888
Brand0nat0r3 <brandon0tor2@gmail.com>
2021-09-28 17:37:25 -0400

    Imported Animations and created a few blank scenes for UI Menus

---
+++ FPS_Survival/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,90 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BUTTON_PRESS = 2698747613U;
+        static const AkUniqueID DOOR_OPENING = 2589897592U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID ROUND_CHANGE = 1930935334U;
+        static const AkUniqueID SHOOT = 3038207054U;
+        static const AkUniqueID TELEPORT = 530129416U;
+        static const AkUniqueID TV_STATIC = 1436086644U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace ALIENS
+        {
+            static const AkUniqueID GROUP = 84587813U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BEEFY_BOIS = 4254775594U;
+                static const AkUniqueID FAST_BOIS = 691572335U;
+                static const AkUniqueID NORMIES = 2122777032U;
+                static const AkUniqueID RANGED = 3686152272U;
+                static const AkUniqueID SPIDER = 1016990062U;
+            } // namespace SWITCH
+        } // namespace ALIENS
+
+        namespace AMBIANCES
+        {
+            static const AkUniqueID GROUP = 1404066300U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AIR = 1050421051U;
+                static const AkUniqueID BUMPS = 3351173968U;
+                static const AkUniqueID COMPUTERS = 1803766445U;
+                static const AkUniqueID CRYO_CHAMBER = 519478265U;
+                static const AkUniqueID SHRIEKS = 978065798U;
+                static const AkUniqueID TV = 1568083719U;
+            } // namespace SWITCH
+        } // namespace AMBIANCES
+
+        namespace INTERACTABLE_OBJECTS
+        {
+            static const AkUniqueID GROUP = 3329721726U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DISPLAY_CASE_PURCHASE = 289811402U;
+                static const AkUniqueID DOORS = 2150196036U;
+                static const AkUniqueID PRINTER = 1425580747U;
+                static const AkUniqueID TELEPORTER = 3373753397U;
+            } // namespace SWITCH
+        } // namespace INTERACTABLE_OBJECTS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MASTER_SOUNDBANK = 2469504869U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
