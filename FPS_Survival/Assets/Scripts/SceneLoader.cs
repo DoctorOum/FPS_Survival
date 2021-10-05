5309527467d7257a7160802f196cbaf7163f6fdb
Brand0nat0r3 <brandon0tor2@gmail.com>
2021-10-05 17:06:16 -0400

    more UI Work

--- FPS_Survival/Assets/Scripts/SceneLoader.cs
+++ FPS_Survival/Assets/Scripts/SceneLoader.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
 
         public void QuitGame()
-        {
+     {
         Application.Quit();
         Debug.Log("Quit!");
-        }
+     }
 
+        public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+        public void CreditsMenu()
+    {
+        SceneManager.LoadScene("Credits Menu");
+    }
+        public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+        public void OptionsMenu()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
 
 }
